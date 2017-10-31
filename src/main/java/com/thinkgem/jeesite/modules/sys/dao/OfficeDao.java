86c46621c30e8ee4e0c8092bdbf493c14eacdad7
shuo.wang@ksudi.com <Wangshuo>
2017-10-31 18:00:12 +0800

    init

---
+++ src/main/java/com/thinkgem/jeesite/modules/sys/dao/OfficeDao.java
@@ -0,0 +1,16 @@
+
+package com.thinkgem.jeesite.modules.sys.dao;
+
+import com.thinkgem.jeesite.common.persistence.TreeDao;
+import com.thinkgem.jeesite.common.persistence.annotation.MyBatisDao;
+import com.thinkgem.jeesite.modules.sys.entity.Office;
+
+/**
+ * 机构DAO接口
+ * @author ThinkGem
+ * @version 2014-05-16
+ */
+@MyBatisDao
+public interface OfficeDao extends TreeDao<Office> {
+	
+}
