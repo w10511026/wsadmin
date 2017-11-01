e38b3d5e696773e0eed2ebea8cccb8f25ffaead8
shuo.wang@ksudi.com <Wangshuo>
2017-11-01 18:40:36 +0800

    add qurtz

---
+++ src/main/java/com/thinkgem/jeesite/common/quartz/ReportScheduledJob.java
@@ -0,0 +1,14 @@
+package com.thinkgem.jeesite.common.quartz;
+
+/**
+ * @Description </br>
+ * @Version Ver 1.0
+ * @Author <a href="mailto:wangshuo@ebnew.com">Wangshuo</a>
+ * @Date 2017/11/1
+ */
+public class ReportScheduledJob {
+
+    public void doSomething() {
+        //System.err.println("****:" + System.currentTimeMillis());
+    }
+}
